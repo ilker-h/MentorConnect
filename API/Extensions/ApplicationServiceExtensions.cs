fbfd6a88cad6a78bcce4b39a9580bd7a80f9df29
ilker-h <ilker.hadzhalaran@gmail.com>
2024-05-31 04:53:36 -0400

    add photo upload, deletion, and cloud storage functionality

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -17,10 +18,12 @@ namespace API.Extensions
             });
 
             services.AddCors();
-            //this is an interface and an implemenetation class. It's best practice to add an interface so that you can write tests more efficiently, due to it isolating where things can go wrong.
+            //this is an interface and an implementation class. It's best practice to add an interface so that you can write tests more efficiently, due to it isolating where things can go wrong.
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>(); // scoped to the level of the HTTP Request
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddScoped<IPhotoService, PhotoService>();
 
             return services;
          }
